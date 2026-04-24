3f837fde3d35c3caa41d1194a56c818450655507
Spencer Baraki <40842515+sbaraki@users.noreply.github.com>
2026-04-23 20:45:34 -0600

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/Scheduler-V1.0/',
+      base: '/SchedulerTool/',
       server: {
         port: 3000,
         host: '0.0.0.0',
